d3c7baa725ec4c6dea7148696c19548dcc8ecd4c
SevenBlack <Tommy@TommydeMacBook-Pro.local>
2017-11-07 21:03:27 +0800

    1107D

--- PhpAction/send.php
+++ PhpAction/send.php
@@ -17,29 +17,29 @@ $mail -> isSMTP();
 // Set mailer to use SMTP
 $mail -> Host = 'smtp.163.com';
 // Specify main and backup SMTP servers
-//$mail -> SMTPAuth = true;
-//// Enable SMTP authentication
-//$mail -> Username = 'qmh0611';
-//// SMTP username
-//$mail -> Password = '76827210qmh';
-//// SMTP password
-//$mail -> CharSet = 'UTF-8';
-//
-//$mail -> setFrom('qmh0611@163.com', 'WebSite');
-//$mail -> addAddress('Tommy@cerasol.com.hk', 'Contact');
-//// Add a recipient
-//
-//$mail -> isHTML(true);
-//// Set email format to HTML
-//
-//$mail -> Subject = '來自H2W網頁的E-mail';
-//$mail -> Body = 'Name:<b>    ' . $name . '</b> <br/><br/>E-mail:<b>    ' . $email . '</b><br/><br/>Message:<b>    ' . $message . '</b>';
-//
-//if (!$mail -> send()) {
-//	echo 'Message could not be sent.';
-//	echo 'Mailer Error: ' . $mail -> ErrorInfo;
-//} else {
-//	echo 'Message has been sent';
-//}
+$mail -> SMTPAuth = true;
+// Enable SMTP authentication
+$mail -> Username = 'qmh0611';
+// SMTP username
+$mail -> Password = '76827210qmh';
+// SMTP password
+$mail -> CharSet = 'UTF-8';
+
+$mail -> setFrom('qmh0611@163.com', 'WebSite');
+$mail -> addAddress('Tommy@cerasol.com.hk', 'Contact');
+// Add a recipient
+
+$mail -> isHTML(true);
+// Set email format to HTML
+
+$mail -> Subject = '來自H2W網頁的E-mail';
+$mail -> Body = 'Name:<b>    ' . $name . '</b> <br/><br/>E-mail:<b>    ' . $email . '</b><br/><br/>Message:<b>    ' . $message . '</b>';
+
+if (!$mail -> send()) {
+	echo 'Message could not be sent.';
+	echo 'Mailer Error: ' . $mail -> ErrorInfo;
+} else {
+	echo 'Message has been sent';
+}
 ?>
 
